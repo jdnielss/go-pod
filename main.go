d98d07f1f58d323ff3f9478edacd552757b67a76
Juan Daniel <mail.jdnielss@gmail.com>
2024-06-03 21:49:01 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"gopkg.in/yaml.v2"
@@ -15,6 +17,8 @@ type Contract struct {
 	Path                 string `yaml:"path"`
 	Method               string `yaml:"method"`
 	ResponseBodyContains string `yaml:"response_body_contains"`
+	RequestBody          string `yaml:"body"`
+	HTTPCodeIs           int    `yaml:"http_code_is"`
 }
 
 type Contracts struct {
@@ -23,7 +27,7 @@ type Contracts struct {
 
 func main() {
 	// Parse command line arguments
-	yamlFilePath := flag.String("f", "config.yaml", "Path to YAML config file")
+	yamlFilePath := flag.String("f", "smoke_test.yaml", "Path to YAML config file")
 	baseURL := flag.String("h", "http://localhost:8080", "Base URL of the API")
 	flag.Parse()
 
@@ -50,13 +54,43 @@ func main() {
 		// Construct full URL
 		fullURL := strings.TrimSuffix(*baseURL, "/") + contract.Path
 
+		// Create the request
+		var req *http.Request
+		var err error
+
+		switch strings.ToUpper(contract.Method) {
+		case "GET":
+			req, err = http.NewRequest(http.MethodGet, fullURL, nil)
+		case "POST":
+			req, err = http.NewRequest(http.MethodPost, fullURL, bytes.NewBufferString(contract.RequestBody))
+		case "PUT":
+			req, err = http.NewRequest(http.MethodPut, fullURL, bytes.NewBufferString(contract.RequestBody))
+		case "DELETE":
+			req, err = http.NewRequest(http.MethodDelete, fullURL, nil)
+		default:
+			fmt.Printf("%s API %s test %s! Unsupported method: %s\n", crossIcon, contract.Name, crossIcon, contract.Method)
+			continue
+		}
+
+		if err != nil {
+			fmt.Printf("%s API %s test %s! Error creating request: %v\n", crossIcon, contract.Name, crossIcon, err)
+			continue
+		}
+
 		// Hit the API
-		resp, err := http.Get(fullURL)
+		client := &http.Client{}
+		resp, err := client.Do(req)
 		if err != nil {
 			fmt.Printf("%s API %s test %s! Error hitting API: %v\n", crossIcon, contract.Name, crossIcon, err)
 			continue
 		}
 		defer resp.Body.Close()
+
+		// Check HTTP status code if specified
+		if contract.HTTPCodeIs != 0 && resp.StatusCode != contract.HTTPCodeIs {
+			fmt.Printf("%s API %s test %s! Expected HTTP code: %d, Actual: %d\n", crossIcon, contract.Name, crossIcon, contract.HTTPCodeIs, resp.StatusCode)
+			continue
+		}
 
 		// Read response body
 		body, err := ioutil.ReadAll(resp.Body)
@@ -66,10 +100,11 @@ func main() {
 		}
 
 		// Check if response body contains expected string
-		if strings.Contains(string(body), contract.ResponseBodyContains) {
-			fmt.Printf("%s ls  %s ll %s!\n", checkIcon, contract.Name, checkIcon)
-		} else {
+		if contract.ResponseBodyContains != "" && !strings.Contains(string(body), contract.ResponseBodyContains) {
 			fmt.Printf("%s API %s test %s! Expected: %s, Actual: %s\n", crossIcon, contract.Name, crossIcon, contract.ResponseBodyContains, string(body))
+			continue
 		}
+
+		fmt.Printf("%s API %s test %s!\n", checkIcon, contract.Name, checkIcon)
 	}
 }
